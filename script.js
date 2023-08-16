6f8cd95051693ad7c6b51f6f3ba107ecf3bed663
Sidopolis <patrosidhu70@gmail.com>
2023-08-16 18:28:24 +0530

    Yes

---
+++ script.js
@@ -0,0 +1,26 @@
+// Pillars of Dom
+
+//1. Selection of an elment
+// 2. Changing Html
+// 3. Changing Style
+// 4. Event Listener 
+
+
+// _______ Changing Html _______ 
+// var a = document.querySelector("h1")
+// a.style.color = "red"
+// a.style.backgroundColor = "yellow"
+
+
+ 
+
+// _______ event Listener _______
+// var a = document.querySelector("h1")
+// a.addEventListener("click", function(){
+//     a.innerHTML = "Badal gaya hu tumhara ex bhi nahi dunga"
+//     a.style.color = "red"
+//     a.style.backgroundColor = "yellow"
+// })
+
+var h = document.querySelector("h1")
+h.textContent = "Bye"
